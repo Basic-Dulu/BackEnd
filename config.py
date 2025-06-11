6568b65a3e379897d4d2af11f17c8fa1be989d49
putrilauraa <rahma.marvialdi@binus.ac.id>
2025-06-12 00:37:41 +0700

    fix(seed): image naming

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import os
 
 class Config:
     # Punya Fatih
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URI", "mysql+mysqlconnector://root:admin@localhost:3306/basic_dulu_db"
-    )
+    # SQLALCHEMY_DATABASE_URI = os.getenv(
+    #     "DATABASE_URI", "mysql+mysqlconnector://root:admin@localhost:3306/basic_dulu_db"
+    # )
 
     # Punya Putri
-    # SQLALCHEMY_DATABASE_URI = os.getenv(
-    #     "DATABASE_URI", "mysql+mysqlconnector://root:root123@localhost:3306/basic_dulu_db"
-    # )
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI", "mysql+mysqlconnector://root:root123@localhost:3306/basic_dulu_db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = os.path.join("static", "uploads")
