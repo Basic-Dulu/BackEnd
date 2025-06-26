f3859bae306360d444e10f23466352eeee0ff1bc
putrilauraa <rahma.marvialdi@binus.ac.id>
2025-06-26 20:25:14 +0700

    fix(dockerfile & run): update for production

--- run.py
+++ run.py
@@ -49,5 +49,5 @@ def create_app():
 # Create the app instance for Gunicorn
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
